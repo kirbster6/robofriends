7b3aef6ea82e8bed0c1b80de7716605eb7c0391b
kirbster6 <kirby.ammari@gmail.com>
2022-07-12 19:54:20 -0700

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'; //Core of the package that does DOM manipulation
+import ReactDOM from 'react-dom/client'; //Used for the DOM/websites, but we can also use React native which would render to mobile applications.
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './containers/App';
+import 'tachyons';
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +15,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
